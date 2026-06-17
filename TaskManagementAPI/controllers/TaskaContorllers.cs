f49fbfc48931bbfca083ad3d3a9e87c32ba39b45
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2026-06-17 21:59:35 +0530

    learn about servises in backend

--- TaskManagementAPI/controllers/TaskaContorllers.cs
+++ TaskManagementAPI/controllers/TaskaContorllers.cs
@@ -1,33 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
-using TaskManagementAPI.Models
+using TaskManagementAPI.Models;
+using TaskManagementAPI.Services;
 
 namespace TaskManagementAPI.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 public class TaskController : ControllerBase{
-    [HttpGet]
-    public IActionResult GetTask(){
-        var tasks = new List<TaskItem>
-        {
-            new(){
-                Id = 1,
-                Title = "Learn Reactjs"
-            },
-            new(){
-                Id = 2,
-                Title = "Learn Nodejs"
-            }
-        };
-        return Ok(tasks);
+    private readonly TaskService _taskService;
+
+    public TaskController(TaskService taskService){
+        _taskService = taskService;
     }
 
-    [HttpGet("{id}")]
-     public IActionResult GetTaskById(int id){
-        var task = new {
-            Id = id,
-            Title = "task found"
-        };
-        return Ok(task);
-     }
+    [HttpGet]
+    public IActionResult GetTasks(){
+        var tasks = _taskService.GetTasks();
+
+        return Ok(tasks);
+    }
 }
